e6366f53f457a569c5a807b738cde8709c59416f
jrb7027 <jrb7027@utulsa.edu>
2024-12-09 18:01:49 -0600

    Chapter 5 Task 5

---
+++ Scripts/pseud_long_quast.sh
@@ -0,0 +1,1 @@
+quast.py --output-dir /scratch/biol726302/BIOL7263_Genomics/pseudomonas_gm41/assembly/hybrid/quast /scratch/biol726302/BIOL7263_Genomics/pseudomonas_gm41/assembly/hybrid/contigs.fasta
